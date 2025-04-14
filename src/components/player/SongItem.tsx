e01c0eb81b3d7f8dfe4ba9b610df296efd5d7e62
KarelPelcak <kaja.pelcak@seznam.cz>
2025-04-14 23:02:09 +0200

    ff

--- src/components/player/SongItem.tsx
+++ src/components/player/SongItem.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-
-import {useAudioPlayer} from "@/context/playerContext";
+import { useAudioPlayer } from "@/context/playerContext";
 
 type Song = {
     title: string;
@@ -9,11 +8,22 @@ type Song = {
 };
 
 export default function SongItemClient({ song }: { song: Song }) {
-    const { playSong } = useAudioPlayer();
+    const { audioRef, setCurrentSong } = useAudioPlayer();
+
+    const handleClick = () => {
+        if (audioRef.current) {
+            audioRef.current.src = song.url;
+            audioRef.current.play().catch(console.error); // ✅ přímá interakce
+        }
+        setCurrentSong(song);
+    };
 
     return (
-        <div onClick={() => playSong(song)} className="cursor-pointer p-1 border-2 border-gray-200">
-            🎵 {song?.title}
+        <div
+            onClick={handleClick}
+            className="cursor-pointer p-2 border-b border-gray-700 hover:bg-gray-800 transition"
+        >
+            🎵 {song.title}
         </div>
     );
 }
