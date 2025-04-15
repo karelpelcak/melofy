ff2f30320b6297e1ba705d0d4e57d53d25f6d497
karel.pelcak <karel.pelcak@regiojet.cz>
2025-04-15 15:40:20 +0200

    New player

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import { useAudioPlayer } from "@/context/playerContext";
+import {useEffect, useRef} from "react";
 
 export default function Player() {
     const { currentSong} = useAudioPlayer();
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    useEffect(() => {
+        if(audioRef.current) {
+            audioRef.current.play();
+        }
+}, [currentSong])
 
     return (
         <div className="absolute w-screen bottom-0 bg-neutral-900 border-t border-neutral-700 px-4 py-3">
             {currentSong && currentSong.title}
-            <audio preload="auto">
+            <audio ref={audioRef} controls>
                 {currentSong?.url && <source src={currentSong.url} type="audio/mpeg" />}
             </audio>
         </div>
